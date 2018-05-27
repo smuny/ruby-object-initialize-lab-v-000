37b1f4f8dbfe3fa0782819341f73ada41173467f
Sharon Mun <sharonymun@gmail.com>
2018-05-27 21:25:06 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,19 @@
 class Dog
+  
   def initialize(name)
     @name = name_of_dog
   end
+  
   def name
     @name
   end
   
   def initialize(breed)
     @breed = breed
-end
-def breed
-  @breed
-end
+  end
+  
+  def breed
+    @breed
+  end
 end
 
